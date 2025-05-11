adcf57dd6a2ac6c48f8ead8985a6f11d0da1d208
arunisin <arunisin.4u@gmail.com>
2025-05-11 14:19:13 +0530

    replaced append with slice

--- go-movies-crud/main.go
+++ go-movies-crud/main.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"strconv"
 
+	"slices"
+
 	"github.com/gorilla/mux"
 )
 
@@ -45,7 +47,7 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for index, item := range movies {
 		if item.ID == params["id"] {
-			movies = append(movies[:index], movies[index+1:]...)
+			movies = slices.Delete(movies, index, index+1)
 			break
 		}
 	}
@@ -60,7 +62,7 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 		if item.ID == params["id"] {
 			var movie Movie
 			_ = json.NewDecoder(r.Body).Decode(&movie)
-			movies = append(movies[:index], movies[index+1:]...)
+			movies = slices.Delete(movies, index, index+1)
 			movie.ID = params["id"]
 			movies = append(movies, movie)
 			json.NewEncoder(w).Encode(movie)
